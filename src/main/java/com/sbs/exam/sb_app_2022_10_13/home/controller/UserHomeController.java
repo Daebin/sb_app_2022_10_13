2830f3cdc172adc422c90c29e1b6dfe32e5f5449
Daebin <201102498@cs-cnu.org>
2022-11-24 20:39:03 +0900

    count 조절 액션

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UserHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UserHomeController.java
@@ -13,39 +13,21 @@ public class UserHomeController {
   public UserHomeController(){
     count=0;
   }
-  @RequestMapping("/usr/home/main")
-  @ResponseBody
-  public String showMain(){
-    return "안녕하세요.";
-  }
 
-
-  @RequestMapping("/usr/home/main2")
+  @RequestMapping("/usr/home/getCount")
   @ResponseBody
-  public String showMain2(){
-    return "반갑습니다.";
-}
-
-  @RequestMapping("/usr/home/main3")
-  @ResponseBody
-  public String showMain3(){
-      return "또 만나요";
-  }
-
-  @RequestMapping("/usr/home/main4")
-  @ResponseBody
-  public int showMain4(){
+  public int getCount(){
 
    count++;
     return count;
   }
 
-  @RequestMapping("/usr/home/main5")
+  @RequestMapping("/usr/home/doSetCount")
   @ResponseBody
-  public String showMain5(){
+  public String doSetCount(int count){
 
-    count =0 ;
-    return "초기화";
+    this.count=count;
+    return "count의 값이"+ this.count+"으로 초기화 되었습니다.";
   }
 }
 
